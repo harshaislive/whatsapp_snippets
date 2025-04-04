99c6839cf1f8fda72fdd6cfd4f287d3ffec5420c
harshaislive <harsha.mudumba@gmail.com>
2025-04-04 17:54:23 +0530

    Fix 502 Bad Gateway error by adding Express server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 8080,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 8080,
+  }
 })
